e259694e5ae493b2b9182ebd3aa75681bb321448
Pranit2811 <77313364+Pranit2811@users.noreply.github.com>
2022-01-18 12:40:11 +0530

    commit

--- base/urls.py
+++ base/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', include('home.urls')),
@@ -29,7 +30,6 @@ urlpatterns = [
     path('logoutuser', include('home.urls')),
     path('delete/<int:id>/', include('home.urls')),
     path('update/<int:id>/', include('home.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
+   
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
